7fb233852116769e2709d70dc171c2e80f243714
James Brown <jbrown@uber.com>
2014-09-29 12:57:55 -0700

    bump to version 0.4.1

---
+++ clusto_query/__init__.py
@@ -0,0 +1,3 @@
+version_info = (0, 4, 1)
+__version__ = '.'.join(str(s) for s in version_info)
+__author__ = 'James Brown <jbrown@uber.com>'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='clusto_query',
-      version='0.4',
+      version='0.4.1',
       author='James Brown',
       author_email='jbrown@uber.com',
       description='Perform arbitrary boolean queries against clusto',
